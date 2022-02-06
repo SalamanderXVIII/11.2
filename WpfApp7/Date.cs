3a10fc3837b9e0e4244511f887840a3bf178e6ea
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2022-02-06 18:51:21 +0200

    Добавьте файлы проекта.

---
+++ WpfApp7/Date.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7
+{
+    public class Date
+    {
+        public int day;
+        public int month;
+        public int year;
+    }
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime date1;
+        Date date = new Date();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            date1 = new DateTime(date.year, date.month, date.day);
+            Output.Text = date1.ToString("d");
+        }
+
+        private void TextDays_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextDay.Text != "")
+            date.day = Convert.ToInt32(TextDay.Text);
+        }
+
+        private void TextMonth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextMonth.Text != "")
+            date.month = Convert.ToInt32(TextMonth.Text);
+        }
+
+        private void TextYears_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextYear.Text != "")
+            date.year = Convert.ToInt32(TextYear.Text);
+        }
+
+        private void Year1_Click(object sender, RoutedEventArgs e)
+        {
+            if (Output.Text != "0")
+            {
+                date1 = date1.AddYears(1);
+                Output.Text = date1.ToString("d");
+            }
+        }
+
+        private void Days2_Click(object sender, RoutedEventArgs e)
+        {
+            if (Output.Text != "0")
+            {
+                date1 = date1.AddDays(-2);
+                Output.Text = date1.ToString("d");
+            }
+        }
+    }
+}
